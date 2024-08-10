87f84f0729d3e23d3722f5991b60eb74e37d8b36
DevSammy22 <sadedeji1988@gmail.com>
2024-08-10 01:35:16 -0400

    refactored the code

--- InPlaceQuickSort.py
+++ InPlaceQuickSort.py
@@ -1,7 +1,7 @@
 def partition(arr, low, high):
     # Choosing the last or rightmost element as the pivot
     pivot = arr[high]
-    # Index of smaller element or Pointer for the smaller element
+    # Start a pointer i at the left boundary
     i = low
     for j in range(low, high):
         # If current element is smaller than or equal to pivot
@@ -9,20 +9,24 @@ def partition(arr, low, high):
             # Swap arr[i] and arr[j]
             arr[i], arr[j] = arr[j], arr[i]
             i += 1
-    # Swap the pivot element with the element at i+1 position
+    # Swap the pivot element with the element at i position
     arr[i], arr[high] = arr[high], arr[i]
 
     return i
 
 def quick_sort(arr, low, high):
+    if len(arr) <= 1:
+        return arr
+
+    # Finding the partitioning index or Choose the rightmost element as the pivot
+    pivot_index = partition(arr, low, high)
+
     if low < high:
-        # Finding the partitioning index or Choose the rightmost element as the pivot
-        pivot_index = partition(arr, low, high)
-
-        # Recursively sort the elements before and after partition
-        quick_sort(arr, low, pivot_index - 1)
-        quick_sort(arr, pivot_index + 1, high)
-
+        # Recursively sort the elements before partition
+        return (arr, low, pivot_index - 1)
+    else:
+        # Recursively sort the elements after partition
+        return quick_sort(arr, pivot_index + 1, high)
 
 
 # Example usage:
